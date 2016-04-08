4eb8e9f44d63a68da28891243afae7e6e550d361
jediazzi <jefferson_diazzi@hotmail.com>
2016-04-08 01:07:17 -0300

    Added files via upload

---
+++ index.php
@@ -0,0 +1,98 @@
+    <?php
+      $cabecalho_title = "Mirror Fashion";
+      include("_cabecalho.php");
+    ?>
+
+    <div class="container destaque">
+
+      <section class="busca">
+        <h2>Busca</h2>
+        <form action="http://www.google.com.br/search" id="form-busca">
+          <input type="search" name="q" id="q">
+          <input type="image" src="img/busca.png">
+        </form>
+      </section>
+
+      <section class="menu-departamentos">
+        <h2>Departamentos</h2>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Blusas e Camisas</a>
+              <ul>
+                <li><a href="#">Manga curta</a></li>
+                <li><a href="#">Manga comprida</a></li>
+                <li><a href="#">Camisa social</a></li>
+                <li><a href="#">Camisa casual</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Calças</a></li>
+            <li><a href="#">Saias</a></li>
+            <li><a href="#">Vestidos</a></li>
+            <li><a href="#">Sapatos</a></li>
+            <li><a href="#">Bolsas e Carteiras</a></li>
+            <li><a href="#">Acessórios</a></li>
+          </ul>
+        </nav>
+      </section>
+
+      <section class="banner">
+        <img src="img/destaque-home.png" alt="Promoção: Big City Night">
+      </section>
+      <section class="botoes-banner">
+        <ul>
+          <li><a href="#"><img src="img/play.png" id="play" alt="Play"></li></a>
+          <li><a href="#"><img src="img/pause.png" id="pause" alt="Pause"></li></a>
+        </ul>
+      </section>
+    </div>
+
+    <div class="container paineis">
+      <section class="painel novidades painel-compacto">
+        <h2>Novidades</h2>
+        <ol>
+          <?php
+            $conexao = mysqli_connect("127.0.0.1", "root", "", "mirror-fashion");
+            $dados = mysqli_query($conexao, "SELECT * FROM produtos ORDER BY data LIMIT 0, 12");
+            while ($produto = mysqli_fetch_array($dados)):
+          ?>
+          <li>
+              <a href="produto.php?id=<?= $produto["id"]?>">
+                <figure>
+                  <img src="img/produtos/miniatura<?= $produto["id"]?>.png" alt="<?= $produto["nome"]?>">
+                  <figcaption><?= $produto["nome"]?> por <?= $produto["preco"]?></figcaption>
+                </figure>
+              </a>
+          </li>
+          <?php endwhile; ?>
+        </ol>
+        <button type="button">Mostra mais</button>
+      </section>
+
+      <section class="painel mais-vendidos painel-compacto">
+        <h2>Mais Vendidos</h2>
+        <ol>
+          <?php
+            $dados = mysqli_query($conexao, "SELECT * FROM produtos ORDER BY vendas LIMIT 0, 12");
+            while ($produto = mysqli_fetch_array($dados)):
+          ?>
+          <li>
+              <a href="produto.php?id=<?= $produto["id"]?>">
+                <figure>
+                  <img src="img/produtos/miniatura<?= $produto["id"]?>.png" alt="<?= $produto["nome"]?>">
+                  <figcaption><?= $produto["nome"]?> por <?= $produto["preco"]?></figcaption>
+                </figure>
+              </a>
+          </li>
+          <?php endwhile; ?>
+        </ol>
+        <button type="button">Mostra mais</button>
+      </section>
+    </div>
+
+    <?php include("_rodape.php");?>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="js/home.js"></script>
+    <script src="js/less.js"></script>
+  </body>
+</html>
---
+++ produto.php
@@ -0,0 +1,90 @@
+<?php
+  $conexao = mysqli_connect("127.0.0.1", "root", "9nirvana0", "WD43");
+  $dados = mysqli_query($conexao, "SELECT * FROM produtos WHERE id = $_GET[id]");
+  while( $produto = mysqli_fetch_array($dados)):
+?>
+
+<?php
+  $cabecalho_title = $produto["nome"];
+  $cabecalho_css = '<link rel="stylesheet" href="css/produto.css">';
+  include("_cabecalho.php");
+?>
+
+    <div class="produto-back">
+      <div class="container">
+        <div class="produto">
+          <h1><?= $produto["nome"]?></h1>
+          <p>por apenas <?= $produto["preco"]?></p>
+
+          <div class="fb-like" data-href="http://mirrorfashion.net/loja"
+              data-layout="standard" data-action="like" data-show-faces="false" data-share="true"></div>
+          <form action="checkout.php" method="POST">
+            <input type="hidden" name="id" value="<?= $produto["id"] ?>">
+
+            <fieldset class="cores">
+              <legend>Escolha a cor:</legend>
+
+              <input type="radio" name="cor" value="verde" id="verde" checked>
+              <label for="verde">
+                <img src="img/produtos/foto<?= $produto["id"] ?>-verde.png">
+              </label>
+
+              <input type="radio" name="cor" value="rosa" id="rosa">
+              <label for="rosa">
+                <img src="img/produtos/foto<?= $produto["id"] ?>-rosa.png">
+              </label>
+
+              <input type="radio" name="cor" value="azul" id="azul">
+              <label for="azul">
+                <img src="img/produtos/foto<?= $produto["id"] ?>-azul.png">
+              </label>
+            </fieldset>
+            <fieldset class="tamanhos">
+              <legend>Escolha o tamanho:</legend>
+              <input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
+              <output for="tamanho" name="valortamanho">42</output>
+            </fieldset>
+
+            <input type="submit" class="comprar" value="Comprar">
+          </form>
+        </div>
+        <div class="detalhes">
+          <h2>Detalhes do produto</h2>
+
+          <p><?= $produto["descricao"]?></p>
+          <table>
+            <thead>
+              <tr>
+                <th>Característica</th>
+                <th>Detalhe</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Modelo</td>
+                <td>Cardigã 7845</td>
+              </tr>
+              <tr>
+                <td>Material</td>
+                <td>Algodão e poliester</td>
+              </tr>
+              <tr>
+                <td>Cores</td>
+                <td>Azul, Rosa e Verde</td>
+              </tr>
+              <tr>
+                <td>Lavagem</td>
+                <td>Lavar a mão</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <div id="fb-root"></div>
+    <?php include("_rodape.php");?>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="js/produto.js"></script>
+    <script src="js/less.js"></script>
+  </body>
+</html>
